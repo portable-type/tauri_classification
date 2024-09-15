4c5914be5b0175c9705c026649234250ae3a2a12
Kazuki Aishima <kazuki.aishima.dev@gmail.com>
2024-09-15 14:02:43 +0900

    predict関数の追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,16 +13,10 @@ use burn::{
     },
     data::dataset::vision::{Annotation, ImageDatasetItem, PixelDepth},
     optim::SgdConfig,
-    prelude::Backend,
 };
 use model::ModelConfig;
 use predict::predict;
 use training::{train, TrainingConfig};
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[tauri::command]
 fn run_train() {
@@ -47,7 +41,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, run_train])
+        .invoke_handler(tauri::generate_handler![run_train, run_predict])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
